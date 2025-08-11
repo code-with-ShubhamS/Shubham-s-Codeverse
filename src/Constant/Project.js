7c9b1197d5fb68b98f273a3ca883510eb2c547df
code-with-ShubhamS <ghostoperator846@gmail.com>
2025-08-11 14:26:32 +0530

    modify project section

--- src/Constant/Project.js
+++ src/Constant/Project.js
@@ -1,4 +1,31 @@
 const ProjectArray =[
+     {
+        tittle:"360 View",
+        description:"A 360 View of images in which you can see the 360 view and go through one image to another. It build with the help of krpano.",
+        image:"./india360.png",
+        technology:["Html","Css","JavaScript","krpano"],
+        publishDate:"1 Auguest 2025",
+        sourceCodeUrl:null,
+        LiveLink:"https://code-with-shubhams.github.io/India360/"
+    },
+     {
+        tittle:"Price Prediction Website",
+        description:"Evolkun is an AI-powered tool that instantly estimates the cost of building a website or digital project. Users just fill out a short form with their requirements, and the AI predicts a fair price based on the project’s scope.",
+        image:"./evolkun.png",
+        technology:["Next js","MongoDB","Tailwand Css" ,"OAuth","OpenAI"],
+        publishDate:"12 June 2025",
+        sourceCodeUrl:null,
+        LiveLink:"https://evolkun.com/"
+    },
+    {
+        tittle:"Storage Website",
+        description:"A Storage Website where you can store your files and access them from anywhere. Just like Google Drive.",
+        image:"./storageApp.png",
+        technology:["React","Node js","MongoDB","Redis","Tailwind css","One tap Login" ,"OAuth", "Admin Pannel","S3 Bucket","Flat Directory Structure"],
+        publishDate:"Still in working..",
+        sourceCodeUrl:"https://github.com/code-with-ShubhamS/Storage-App.git",
+        LiveLink:null
+    },
     {
         tittle:"Youtube Clone",
         description:"A Website which is like as Youtube you can Watch Videos and new trending videos in this website.",
@@ -17,6 +44,7 @@ const ProjectArray =[
         sourceCodeUrl:"https://github.com/code-with-ShubhamS/Music-App",
         LiveLink:null
     },
+   
    
   
 ]
--- src/components/ProjectData.jsx
+++ src/components/ProjectData.jsx
@@ -11,7 +11,6 @@ const ProjectData = ({
   liveDemoUrl,
   technology,
 }) => {
- 
   return (
     <div className="flex flex-col  my-7 justify-center sm:flex-row">
       <div className="flex p-4 w-[100%]  sm:w-[50%]">
@@ -28,8 +27,8 @@ const ProjectData = ({
           <Safari
             imageSrc={image}
             url={title}
-            mode="simple"
-            className="size-full transition-transform duration-500 group-hover:scale-105 group-hover:shadow-xl "
+            // mode="simple"
+            className="size-full transition-transform duration-500 group-hover:scale-105 group-hover:shadow-xl object-cover "
           />
           {/* Optional overlay effect */}
           {/* <div className="absolute inset-0 bg-black/5 opacity-0 group-hover:opacity-100 rounded-lg transition-opacity duration-500"></div> */}
@@ -61,33 +60,26 @@ const ProjectData = ({
 
         {/* Buttons */}
         <div className="flex space-x-4">
-          <a
-            href={sourceCodeUrl}
-            target="_blank"
-            className={`flex items-center space-x-2 bg-gray-900  py-2 px-4 rounded-lg hover:bg-gray-800 transition duration-300`}
-          >
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2 0-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.96 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.09 0 0 .67-.22 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.08.16 1.89.08 2.09.51.56.82 1.27.82 2.15 0 3.08-1.87 3.75-3.65 3.96.29.25.54.73.54 1.47 0 1.07-.01 1.93-.01 2.19 0 .21.15.45.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z" />
-            </svg>
-            <span>Source Code</span>
-          </a>
+          
+            <a
+              href={sourceCodeUrl}
+              target="_blank"
+              className={`flex items-center space-x-2 bg-gray-900  py-2 px-4 rounded-lg hover:bg-gray-800 transition duration-300`}
+            >
+              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2 0-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.96 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.09 0 0 .67-.22 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.08.16 1.89.08 2.09.51.56.82 1.27.82 2.15 0 3.08-1.87 3.75-3.65 3.96.29.25.54.73.54 1.47 0 1.07-.01 1.93-.01 2.19 0 .21.15.45.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+          <span>  {sourceCodeUrl === null ? "N/A" : "Source Code" }</span>
+              
+            </a>
+          
 
           <a
             href={liveDemoUrl}
             target="_blank"
             className={`flex items-center space-x-2 bg-white text-black py-2 px-4 rounded-lg hover:bg-gray-200 transition duration-300`}
           >
-            <span>{liveDemoUrl === null ? "Not Live" : "Live Demo"}</span>
-            {/* <svg
-              className="w-5 h-5 mb-[6px] "
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                className=""
-                d="M12.293 8.293a1 1 0 0 1 1.414 1.414L10.414 13l3.293 3.293a1 1 0 1 1-1.414 1.414L8 14.414l-3.293 3.293a1 1 0 0 1-1.414-1.414L6.586 13 3.293 9.707a1 1 0 0 1 1.414-1.414L8 11.586l3.293-3.293z"
-              />
-            </svg> */}
+            <span>{liveDemoUrl === null ? "N/A" : "Live Demo"}</span>
           </a>
         </div>
       </div>
